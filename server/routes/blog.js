5aaf02f0c97b036bc75ec188f974c8d5a1f97a66
aleortbas <aleortbas@gmail.com>
2024-01-21 19:22:40 -0500

    blog query

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -8,19 +8,20 @@ const router = express.Router();
 
 router.route("/blogsHome").get(async (req, res) => {
   try {
-    const query = "SELECT * FROM post";
+    const query =
+      "SELECT  b.title, b.headline, TO_CHAR(b.published_date, 'DD-MM-YYYY') as published_date, b.body, pc.category_name, u.user FROM post as b INNER JOIN post_categories as pc ON pc.id_category = b.category_id INNER JOIN users as u ON u.id_user = b.user_id";
     db.pool.query(query, (err, result) => {
       if (err) {
         return res.status(500).json({ message: "Error querying the database" });
       }
       if (result.rows.length === 1) {
         const title = result.rows[2];
-        console.log("title: ", title);
       }
-      console.log(result.rows[0]);
-      res.status(200).json({ user: result.rows[0] });
+      res.status(200).json({ blog: result.rows });
     });
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error " });
+  }
 });
 
 module.exports = router;
