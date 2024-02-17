4d99ab2c1d86a42b23f6c22febbd074dbcccac97
aleortbas <aleortbas@gmail.com>
2024-02-17 15:32:46 -0500

    uploading buffer image

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -2,6 +2,9 @@ const db = require("../dbConnection");
 const bodyParser = require("body-parser");
 const authMiddleware = require("../middleware");
 const express = require("express");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 require("dotenv").config();
 const router = express.Router();
@@ -9,7 +12,7 @@ const router = express.Router();
 router.route("/blogsHome").get(async (req, res) => {
   try {
     const query =
-      "SELECT  b.title, b.headline, TO_CHAR(b.published_date, 'DD-MM-YYYY') as published_date, b.body, pc.category_name, u.user FROM post as b INNER JOIN post_categories as pc ON pc.id_category = b.category_id INNER JOIN users as u ON u.id_user = b.user_id";
+      "SELECT  b.title, b.headline, TO_CHAR(b.published_date, 'DD-MM-YYYY') as published_date, b.body, pc.category_name, b.images, u.user FROM post as b INNER JOIN post_categories as pc ON pc.id_category = b.category_id INNER JOIN users as u ON u.id_user = b.user_id";
     db.pool.query(query, (err, result) => {
       if (err) {
         return res.status(500).json({ message: "Error querying the database" });
@@ -25,11 +28,12 @@ router.route("/blogsHome").get(async (req, res) => {
 });
 
 router.route("/postBlog").post((req, res) => {
-  const { user_id, titleT, headlineT, body } = req.body;
+  const { user_id, titleT, headlineT, body, imageBlob } = req.body;
   console.log("USER", user_id);
+  console.log("images", imageBlob);
   try {
     const query =
-      "INSERT INTO post (user_id, title, headline, published_date, body, category_id) VALUES ($1, $2, $3,NOW(), $4, 1)";
+      "INSERT INTO post (user_id, title, headline, published_date, body, category_id, images) VALUES ($1, $2, $3,NOW(), $4, 1, $5)";
     const values = [user_id, titleT, headlineT, body];
     db.pool.query(query, values, (err, result) => {
       if (err) {
@@ -40,6 +44,23 @@ router.route("/postBlog").post((req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
+});
+
+router.post("/uploadImage", upload.single("file"), (req, res, next) => {
+  //res.json(req.file);
+  const file = req.file;
+  const query =
+    "INSERT INTO post (user_id, title, headline, published_date, body, category_id, images) VALUES (124, $1, '$3',NOW(), '$4', 1, $2)";
+  const image = file.buffer.toString("base64");
+  const values = [file.originalname, image];
+  db.pool.query(query, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: "Error" });
+    } else {
+      res.status(200).json({ message: "Upload" });
+    }
+  });
 });
 
 module.exports = router;
