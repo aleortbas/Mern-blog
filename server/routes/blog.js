541a2071c933a63d5c9d0240a20ef405b2464af7
aleortbas <aleortbas@gmail.com>
2024-02-24 16:55:05 -0500

    upload metadata and save images working

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -3,16 +3,26 @@ const bodyParser = require("body-parser");
 const authMiddleware = require("../middleware");
 const express = require("express");
 const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const fs = require("fs");
 
 require("dotenv").config();
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/home/aleortbas/Documents/imagesBlog");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 router.route("/blogsHome").get(async (req, res) => {
   try {
     const query =
-      "SELECT  b.title, b.headline, TO_CHAR(b.published_date, 'DD-MM-YYYY') as published_date, b.body, pc.category_name, b.images, u.user FROM post as b INNER JOIN post_categories as pc ON pc.id_category = b.category_id INNER JOIN users as u ON u.id_user = b.user_id";
+      "SELECT  b.title, b.headline, TO_CHAR(b.published_date, 'DD-MM-YYYY') as published_date, b.body, pc.category_name, u.user FROM post as b INNER JOIN post_categories as pc ON pc.id_category = b.category_id INNER JOIN users as u ON u.id_user = b.user_id";
     db.pool.query(query, (err, result) => {
       if (err) {
         return res.status(500).json({ message: "Error querying the database" });
@@ -28,12 +38,10 @@ router.route("/blogsHome").get(async (req, res) => {
 });
 
 router.route("/postBlog").post((req, res) => {
-  const { user_id, titleT, headlineT, body, imageBlob } = req.body;
-  console.log("USER", user_id);
-  console.log("images", imageBlob);
+  const { user_id, titleT, headlineT, body } = req.body;
   try {
     const query =
-      "INSERT INTO post (user_id, title, headline, published_date, body, category_id, images) VALUES ($1, $2, $3,NOW(), $4, 1, $5)";
+      "INSERT INTO post (user_id, title, headline, published_date, body, category_id) VALUES ($1, $2, $3,NOW(), $4, 1)";
     const values = [user_id, titleT, headlineT, body];
     db.pool.query(query, values, (err, result) => {
       if (err) {
@@ -47,20 +55,22 @@ router.route("/postBlog").post((req, res) => {
 });
 
 router.post("/uploadImage", upload.single("file"), (req, res, next) => {
-  //res.json(req.file);
-  const file = req.file;
-  const query =
-    "INSERT INTO post (user_id, title, headline, published_date, body, category_id, images) VALUES (124, $1, '$3',NOW(), '$4', 1, $2)";
-  const image = file.buffer.toString("base64");
-  const values = [file.originalname, image];
-  db.pool.query(query, values, (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).json({ message: "Error" });
-    } else {
-      res.status(200).json({ message: "Upload" });
-    }
-  });
+  const { originalname, size, path } = req.file;
+  console.log(path);
+
+  try {
+    const query =
+      "INSERT INTO public.images(id_image, id_blog, filename, file_size, file_path) VALUES (2, 20, $1, $2, $3);";
+    const values = [originalname, size, path];
+    db.pool.query(query, values, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200).json({ message: "image data added" });
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
 });
 
 module.exports = router;
