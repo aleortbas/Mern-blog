1dcaa299e69cf727544570678c7d2f90c1702a0e
aleortbas <aleortbas@gmail.com>
2024-04-07 22:15:58 -0500

    inser into blogs working without fixed data

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -23,7 +23,7 @@ const upload = multer({ storage: storage });
 router.route("/blogsHome").get(async (req, res) => {
   try {
     const query =
-      "SELECT  b.title, b.headline, TO_CHAR(b.published_date, 'DD-MM-YYYY') as published_date, b.body, pc.category_name, u.user FROM post as b INNER JOIN post_categories as pc ON pc.id_category = b.category_id INNER JOIN users as u ON u.id_user = b.user_id";
+      "SELECT b.id_post, b.title, b.headline, TO_CHAR(b.published_date, 'DD-MM-YYYY') as published_date, b.body, pc.category_name, u.user FROM post as b INNER JOIN post_categories as pc ON pc.id_category = b.category_id INNER JOIN users as u ON u.id_user = b.user_id";
     db.pool.query(query, (err, result) => {
       if (err) {
         return res.status(500).json({ message: "Error querying the database" });
@@ -45,16 +45,33 @@ router.post("/uploadImage", upload.single("file"), (req, res, next) => {
   console.log(user_id);
   try {
     const queryImage =
-      "INSERT INTO public.images(id_image, id_blog, filename, file_size, file_path) VALUES (2, 20, $1, $2, $3);";
-    const valuesImage = [originalname, size, path];
-    db.pool.query(queryImage, valuesImage);
+      "INSERT INTO public.images(id_blog, filename, file_size, file_path) VALUES ($4, $1, $2, $3);";
 
     const queryBlog =
-      "INSERT INTO post (user_id, title, headline, published_date, body, category_id) VALUES ($1, $2, $3,NOW(), $4, 1)";
-    const values = [user_id, titleT, headlineT, body];
-    db.pool.query(queryBlog, values);
+      "INSERT INTO post (user_id, title, headline, published_date, body, category_id) VALUES ($1, $2, $3,NOW(), $4, 1) RETURNING id_post";
 
-    res.status(200).json({ message: "image data added" });
+    const valuesBlog = [user_id, titleT, headlineT, body];
+
+    const valuesImage = [originalname, size, path];
+
+    db.pool.query(queryBlog, valuesBlog, (error, results) => {
+      if (error) {
+        console.error("Error occurred Blog: ", error);
+      } else {
+        const postId = results.rows[0];
+        valuesImage.push(postId.id_post);
+        console.log("AQUI ", valuesImage);
+        db.pool.query(queryImage, valuesImage, (error, results) => {
+          if (error) {
+            console.error("Error occurred Image: ", error);
+          } else {
+            console.log("POst and Image inserted succesfully");
+          }
+        });
+      }
+    });
+
+    res.status(200).json({ message: "image data added", id: "id_post" });
   } catch (error) {
     res.status(500).json({ message: "server error" });
   }
